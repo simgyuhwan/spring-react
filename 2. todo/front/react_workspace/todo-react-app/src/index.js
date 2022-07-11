04a5183d3683f5c64c10a372fc75d462aab2a17a
simgyuhwan <37525159+simgyuhwan@users.noreply.github.com>
2022-07-12 00:22:42 +0900

    front 미비

--- 2. todo/front/react_workspace/todo-react-app/src/index.js
+++ 2. todo/front/react_workspace/todo-react-app/src/index.js
@@ -1,17 +1,22 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import Todo from "./Todo";
+import './App.css'
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+class App extends React.Component{
+  constructor(props){
+    super(props);
+    this.state = {
+      item: {id:0, title: "hello world 1", done: true},
+    };
+  }
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+  render(){
+    return (
+      <div className="App">
+        <Todo item={this.state.item} />
+      </div>
+    );
+  }
+}
+
+export default App;
